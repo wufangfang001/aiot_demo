e2628525cf88a436b100d450e6cd8bf24809e31b
wufangfang <wufangfang@agora.io>
2026-06-29 17:29:12 +0800

    update build scripts

--- build_rtsa_demo.sh
+++ build_rtsa_demo.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
 CC=${CC:-gcc}
-PLATFORM="$1"
-if [[ -z "$PLATFORM" || ("$PLATFORM" != "x86" && "$PLATFORM" != "arm") ]]; then
-    echo "Usage: $0 [x86|arm]"
+PLATFORM="${1:-}"
+BUILD_TYPE="${2:-release}"
+if [[ -z "$PLATFORM" ]]; then
+    machine="$(uname -m)"
+    case "$machine" in
+        x86_64|i386|i686|i?86)
+            PLATFORM="x86"
+            ;;
+        aarch64|arm64|armv7l|armv8l)
+            PLATFORM="arm"
+            ;;
+        *)
+            echo "Unable to detect platform from uname -m: $machine"
+            echo "Usage: $0 [x86|arm] [release|debug]"
+            exit 1
+            ;;
+    esac
+fi
+if [[ "$PLATFORM" != "x86" && "$PLATFORM" != "arm" ]]; then
+    echo "Usage: $0 [x86|arm] [release|debug]"
     exit 1
+fi
+
+if [[ "$BUILD_TYPE" == "debug" || "${DEBUG:-0}" == "1" ]]; then
+    CFLAGS="-g -O0"
+else
+    CFLAGS="-Werror -O2"
 fi
 
 RPATH_DIR="./lib/$PLATFORM"
 
-$CC -Werror -O2 -o audio-call                          \
+$CC $CFLAGS -o audio-call                          \
     src/amf_ringbuf.c                                  \
     src/audio_playback_wrapper_portaudio_impl.c        \
     src/audio_capture_wrapper_portaudio_impl.c         \
